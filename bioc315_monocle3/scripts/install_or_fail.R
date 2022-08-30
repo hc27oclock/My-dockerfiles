39cc1783065a04df6d9878209ea7218f30f2c9e4
Hyunwoo Cho <hc27oclock@gmail.com>
2022-08-30 19:35:28 -0400

    Update install_or_fail.R

--- bioc315_monocle3/scripts/install_or_fail.R
+++ bioc315_monocle3/scripts/install_or_fail.R
@@ -1,5 +1,5 @@
 #!/usr/bin/env Rscript
-install_or_fail = function(package_name) {
+cran_install_or_fail = function(package_name) {
   tryCatch({
     install.packages(package_name, dependencies=T);
     library(package_name)},
@@ -16,3 +16,45 @@ install_or_fail = function(package_name) {
     }
   )
 }
+
+bioc_install_or_fail = function(package_name) {
+  tryCatch({
+    BiocManager::install(package_name, dependencies=T);
+    library(package_name)},
+    error=function(e) {print(e)},
+    warning=function(w) {
+      catch = grepl("download of package .* failed", w$message) ||
+        grepl("(dependenc|package).*(is|are) not available", w$message) ||
+        grepl("installation of package.*had non-zero exit status", w$message) ||
+        grepl("installation of one or more packages failed", w$message)
+      if (catch) {
+        print(w$message);
+        stop(paste0("installation failed for: ", package_name))
+      }
+    }
+  )
+}
+
+devtools_install_or_fail = function(package_name) {
+  tryCatch({
+    devtools::install_github(package_name, dependencies=T);
+    library(package_name)},
+    error=function(e) {print(e)},
+    warning=function(w) {
+      catch = grepl("download of package .* failed", w$message) ||
+        grepl("(dependenc|package).*(is|are) not available", w$message) ||
+        grepl("installation of package.*had non-zero exit status", w$message) ||
+        grepl("installation of one or more packages failed", w$message)
+      if (catch) {
+        print(w$message);
+        stop(paste0("installation failed for: ", package_name))
+      }
+    }
+  )
+}
+
+cran_install_or_fail(c("devtools", "BiocManager"))
+bioc_install_or_fail(c("BiocGenerics", "DelayedArray", "DelayedMatrixStats", "limma", "lme4", "S4Vectors", "SingleCellExperiment", "SummarizedExperiment", "batchelor", "ggrastr", "remotes", "Seurat"))
+devtools_install_or_fail("cole-trapnell-lab/leidenbase")
+devtools_install_or_fail("cole-trapnell-lab/monocle3")
+
