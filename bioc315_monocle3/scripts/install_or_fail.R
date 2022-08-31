b48dade8e02914686da2cb8203873d0dff7eca76
Hyunwoo Cho <hc27oclock@gmail.com>
2022-08-30 20:04:42 -0400

    Update install_or_fail.R

--- bioc315_monocle3/scripts/install_or_fail.R
+++ bioc315_monocle3/scripts/install_or_fail.R
@@ -1,7 +1,17 @@
 #!/usr/bin/env Rscript
-cran_install_or_fail = function(package_name) {
+install_or_fail = function(package_name, pack="cran") {
+  if (pack == "cran") {
+    installfunc = "install.packages"
+  }
+  if (pack == "bioc") {
+    installfunc = "BiocManager::install"
+  }
+  if (pack == "devtools") {
+    installfunc = "devtools::install_github"
+  }
+
   tryCatch({
-    install.packages(package_name, dependencies=NA);
+    eval(parse(text=paste0(installfunc, "(\"", package_name, "\", dependencies=NA)")));
     library(package_name)},
     error=function(e) {print(e)},
     warning=function(w) {
@@ -17,44 +27,8 @@ cran_install_or_fail = function(package_name) {
   )
 }
 
-bioc_install_or_fail = function(package_name) {
-  tryCatch({
-    BiocManager::install(package_name, dependencies=NA);
-    library(package_name)},
-    error=function(e) {print(e)},
-    warning=function(w) {
-      catch = grepl("download of package .* failed", w$message) ||
-        grepl("(dependenc|package).*(is|are) not available", w$message) ||
-        grepl("installation of package.*had non-zero exit status", w$message) ||
-        grepl("installation of one or more packages failed", w$message)
-      if (catch) {
-        print(w$message);
-        stop(paste0("installation failed for: ", package_name))
-      }
-    }
-  )
-}
-
-devtools_install_or_fail = function(package_name) {
-  tryCatch({
-    devtools::install_github(package_name, dependencies=NA);
-    library(package_name)},
-    error=function(e) {print(e)},
-    warning=function(w) {
-      catch = grepl("download of package .* failed", w$message) ||
-        grepl("(dependenc|package).*(is|are) not available", w$message) ||
-        grepl("installation of package.*had non-zero exit status", w$message) ||
-        grepl("installation of one or more packages failed", w$message)
-      if (catch) {
-        print(w$message);
-        stop(paste0("installation failed for: ", package_name))
-      }
-    }
-  )
-}
-
-cran_install_or_fail(c("BiocManager", "devtools"))
-bioc_install_or_fail(c("BiocGenerics", "DelayedArray", "DelayedMatrixStats", "limma", "lme4", "S4Vectors", "SingleCellExperiment", "SummarizedExperiment", "batchelor", "ggrastr", "remotes", "Seurat"))
-devtools_install_or_fail("cole-trapnell-lab/leidenbase")
-devtools_install_or_fail("cole-trapnell-lab/monocle3")
+install_or_fail(c("BiocManager", "devtools"), pack="cran")
+install_or_fail(c("BiocGenerics", "DelayedArray", "DelayedMatrixStats", "limma", "lme4", "S4Vectors", "SingleCellExperiment", "SummarizedExperiment", "batchelor", "ggrastr", "remotes", "Seurat"), pack="bioc")
+install_or_fail("cole-trapnell-lab/leidenbase", pack="devtools")
+install_or_fail("cole-trapnell-lab/monocle3", pack="devtools")
 
