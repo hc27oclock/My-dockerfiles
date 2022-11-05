d48248ce12eb95f6aea8e34ecde8421d7a15e446
Hyunwoo Cho <hc27oclock@gmail.com>
2022-11-05 14:38:56 -0400

    Update install_r_packages.R

--- mambaforge_r/install_r_packages.R
+++ mambaforge_r/install_r_packages.R
@@ -1,5 +1,5 @@
 #!/usr/bin/env Rscript
 
 BiocManager::install(c("BSgenome.Hsapiens.UCSC.hg19"), update=F)
-devtools::install_github("cole-trapnell-lab/leidenbase", quick=T, dependencies="Depends", upgrade_dependencies=F)
+devtools::install_github("cole-trapnell-lab/leidenbase", quick=T, dependencies="Depends", upgrade_dependencies=T)
 devtools::install_github("cole-trapnell-lab/monocle3", quick=T, dependencies="Depends", upgrade_dependcies=F)
