1ccd05e483736b20e3a2060a4ba783c29a1cfb86
Hyunwoo Cho <hc27oclock@gmail.com>
2022-11-05 15:33:56 -0400

    Update install_r_packages.R

--- mambaforge_r/install_r_packages.R
+++ mambaforge_r/install_r_packages.R
@@ -1,9 +1,8 @@
 #!/usr/bin/env Rscript
 
-BiocManager::install(c("BSgenome.Hsapiens.UCSC.hg19"), update=F)
+BiocManager::install(c("BSgenome.Hsapiens.UCSC.hg19"))
 BiocManager::install(c('BiocGenerics', 'DelayedArray', 'DelayedMatrixStats',
                        'limma', 'lme4', 'S4Vectors', 'SingleCellExperiment',
                        'SummarizedExperiment', 'batchelor', 'Matrix.utils',
                        'HDF5Array', 'terra', 'ggrastr'))
-# devtools::install_github("cole-trapnell-lab/leidenbase", quick=T)
-devtools::install_github("cole-trapnell-lab/monocle3", quick=T)
+devtools::install_github("cole-trapnell-lab/monocle3")
