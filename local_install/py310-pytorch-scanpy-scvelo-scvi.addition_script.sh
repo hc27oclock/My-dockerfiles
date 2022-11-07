27276286132154a46b938cbcacef0083287c8996
Hyunwoo Cho <hc27oclock@gmail.com>
2022-11-07 10:44:40 -0500

    Update py310-pytorch-scanpy-scvelo-scvi.addition_script.sh

--- local_install/py310-pytorch-scanpy-scvelo-scvi.addition_script.sh
+++ local_install/py310-pytorch-scanpy-scvelo-scvi.addition_script.sh
@@ -1,3 +1,4 @@
 # conda activate pytorch
-mamba install pytorch torchvision torchaudio -c pytorch
+mamba update pytorch torchvision torchaudio -c pytorch
 python -m pip install --upgrade git+https://github.com/theislab/scvelo.git
+# mamba update -y --all && mamba upgrade -y --all && conda clean -y --all
