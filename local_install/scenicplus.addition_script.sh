14765bdbb01ef3c849202e7f498a65df7f7379f1
Hyunwoo Cho <hc27oclock@gmail.com>
2022-11-07 10:50:57 -0500

    Create scenicplus.addition_script.sh

---
+++ local_install/scenicplus.addition_script.sh
@@ -0,0 +1,3 @@
+# conda activate scenicplus
+python -m pip install --upgrade git+https://github.com/aertslab/scenicplus.git
+# mamba update -y --all && mamba upgrade -y --all && conda clean -y --all
